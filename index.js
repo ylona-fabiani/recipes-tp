dbf9d78396671de72eabc59455bd80eef50d97e4
ylow <ylona.fabiani@etu.umontpellier.fr>
2021-12-16 12:28:38 +0100

    get recipes and users + almost create recipes

--- index.js
+++ index.js
@@ -2,13 +2,31 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000 // this is very important
 const axios = require('axios')
-const nunjucks = require('nunjucks')
+const api_key = '04c547857520b4a9bbe5ee0d32db370e6cfed'
 
+app.get('/recipes', async function (req, res) {
+  const db = await axios.get('https://recipestp-2fc5.restdb.io/rest/r-recipes', { headers : {'x-api-key' : api_key} })
+  let qTitle = db.data.map(e => {return {"id" : e._id, "title" : e.title}}); // map renvoie un nvo tableau de ce qu'on veut 
+                                                              // on,peut apres faire un foreach dessus pour les titles en li
+  res.json(qTitle)
+})
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/users', async function (req, res) {
+  const db = await axios.get('https://recipestp-2fc5.restdb.io/rest/r-users', { headers : {'x-api-key' : api_key} })
+  console.log(db.data);
+  res.send(db.data)
 })
 
 app.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT)
+})
+
+app.post('/recipes', express.json(), async function(req,res){
+  const db = await axios.post('https://recipestp-2fc5.restdb.io/rest/recipes', req.body,{
+    headers: {
+      "x-api-key": api_key
+    }
+  })
+
+  res.send('Recette Créée')
 })
