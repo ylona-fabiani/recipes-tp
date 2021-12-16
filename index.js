5895ed60ac4b03c50b1498a8198b2599c2ae5da3
ylow <ylona.fabiani@etu.umontpellier.fr>
2021-12-16 11:01:58 +0100

    added require

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000 // this is very important
-
 const axios = require('axios')
+const nunjucks = require('nunjucks')
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
