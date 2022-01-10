ee9b599bcc05920937197fe63dd62bd71027f401
ROURE Elie <elie.roure@etu.umontpellier.fr>
2022-01-10 09:07:00 +0100

    heuuuuuuuu

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const PORT = process.env.PORT || 5000 // this is very important
 const axios = require('axios')
 const api_key = '04c547857520b4a9bbe5ee0d32db370e6cfed'
 
+app.listen(PORT, function () {
+  console.log('Example app listening on port ' + PORT)
+})
+
+//READ
+
 app.get('/recipes', async function (req, res) {
   const db = await axios.get('https://recipestp-2fc5.restdb.io/rest/r-recipes', { headers : {'x-api-key' : api_key} })
   let qTitle = db.data.map(e => {return {"id" : e._id, "title" : e.title}}); // map renvoie un nvo tableau de ce qu'on veut 
@@ -17,16 +23,25 @@ app.get('/users', async function (req, res) {
   res.send(db.data)
 })
 
-app.listen(PORT, function () {
-  console.log('Example app listening on port ' + PORT)
-})
-
+//CREATE
 app.post('/recipes', express.json(), async function(req,res){
   const db = await axios.post('https://recipestp-2fc5.restdb.io/rest/recipes', req.body,{
     headers: {
       "x-api-key": api_key
     }
   })
-
   res.send('Recette Créée')
 })
+
+app.post('/users', express.json(), async function(req,res){
+  const db = await axios.post('https://recipestp-2fc5.restdb.io/rest/recipes', req.body,{
+    headers: {
+      "x-api-key": api_key
+    }
+  })
+  res.send('Recette Créée')
+})
+
+//UPDATE
+
+//DELETE
