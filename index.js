c5259f08b15c1263a68e933cff3a83b5a8609004
ylow <ylona.fabiani@etu.umontpellier.fr>
2022-02-17 11:08:48 +0100

    get users modified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 5000 // this is very important
+const PORT = process.env.PORT || 62000 // this is very important
 const axios = require('axios')
 const api_key = '04c547857520b4a9bbe5ee0d32db370e6cfed'
 const urlDb = 'https://recipestp-2fc5.restdb.io/rest/'
@@ -79,16 +79,12 @@ app.post('/recipes', passport.authenticate('jwt', { session: false }), express.j
 app.post('/users', express.json(), async function (req, res) {
   console.log(req.body);
   try {
-    const db = await axios.post(`${urlDb}r-users`, req.body, {
-      headers: {
-        "x-api-key": api_key
-      }
-    })
-    res.send('Utilisateur Créée')
+    const db = await axios.post(`${urlDb}r-users`, req.body, { headers: { "x-api-key": api_key }})
+    res.send('Utilisateur Créé')
   }
   catch(err) {
     //console.log(err)
-    res.send('pb telephone')
+    res.send('problème')
   }
 })
 
